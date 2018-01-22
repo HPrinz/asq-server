2f0202e426ee78e20b9625d95c3e8bcb3b96c5a2
Hanna Prinz <hanna_prinz@yahoo.de>
2018-01-22 19:02:52 +0100

    no message

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # asq-server
-Kotlin server application for ASQ* App 
+
+Kotlin & Spring Boot server application for ASQ* App
+
+## TODOs
+- [x] GET all Questions
+- [x] PUT new Question
+- [x] store questions somehow (cache)
+- [ ] GET only Questions of others
+- [ ] GET only users Questions
+- [ ] extend test coverage
+- [ ] save questions in db
+- [x] continous integration
+- [x] setup tests
+- [ ] automate test execution (gradle)
+- [ ] GET categories
+- [ ] automatically assign question to category
+
+## Maybe
--- src/main/kotlin/asq/QuestionsController.kt
+++ src/main/kotlin/asq/QuestionsController.kt
@@ -14,14 +14,8 @@ class QuestionsController() {
 
     val questionsDao: QuestionsDAO = QuestionsListImpl()
 
-//    @GetMapping()
-//    fun hello(@RequestParam(value = "name", defaultValue = "World") name: String) =
-//            Greeting(counter.incrementAndGet(), "Hello, $name")
-
     @GetMapping()
     fun getQuestions(): List<Question> {
-//        val question = Question(counter.incrementAndGet(), "Wo gibt es gutes Essen in Berlin?", "user1", Instant.now())
-//        questionsDao.saveQuestion(question)
         return questionsDao.getQuestions()
     }
 
